a24669ce5ba051f4422ac9c18b33f7182d2d1147
Vershima kwaghshi <kwaghshivershimasolomon@gmail.com>
2026-07-09 11:04:39 +0100

    Implement alert on waitlist form submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+alert("Thank you for joining the Take Profit waitlist.");
+
+});
